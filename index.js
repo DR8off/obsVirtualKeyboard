425900076e0ec3101ba3ab78cb0e3af34bae52b5
BerezkinDev <berezkindev@gmail.com>
2025-01-16 07:38:45 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const keyboardKeys = ['w','a','s','d']
+const keyboard = document.createElement('div')
+keyboard.classList.add('keyboard')
+document.body.appendChild(keyboard)
+
+keyboardKeys.forEach(key => {
+    const keyboardKey = document.createElement('div')
+    keyboardKey.classList.add('keyboardKey', key)
+    keyboardKey.textContent = key
+
+    keyboard.appendChild(keyboardKey)
+})
+
+document.addEventListener('keydown', (e) => {
+    e.preventDefault()
+
+    const { key } = e
+    document.querySelector(`.${key}`).classList.add('active')
+})
+document.addEventListener('keyup', (e) => {
+    e.preventDefault()
+
+    const { key } = e
+    document.querySelector(`.${key}`).classList.remove('active')
+})
